f3913d96e2774909d8d49b6adf572b154d3e53ed
Eyal Ellenbogen <eyale@outlook.com>
2021-06-16 17:32:38 +0300

    update deps

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-    "roots": [
-      "<rootDir>/src"
-    ],
-    "testMatch": [
-      "**/?(*.)+(spec|test).+(ts|tsx|js)"
-    ],
-    "transform": {
-      "^.+\\.(ts|tsx)$": "ts-jest"
-    },
-  }
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};

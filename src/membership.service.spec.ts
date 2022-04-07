015782d0b99d7c7e77b20ad04b8ba7a0d8376f67
eellenbogen <eellenbogen@varonis.com>
2022-04-07 10:58:58 +0300

    update texts for clarity

--- src/membership.service.spec.ts
+++ src/membership.service.spec.ts
@@ -1,77 +1,68 @@
-import { ProxyServiceMock } from "./proxy.service.mock";
-import { MembershipService } from "./membership.service";
-describe("MembershipService getUsers() method", () => {
+import { ProxyServiceMock } from './proxy.service.mock';
+import { MembershipService } from './membership.service';
+describe('MembershipService getUsers() method', () => {
   let proxy = ProxyServiceMock;
   let proxySpy: jest.SpyInstance<Promise<IUser[]>>;
   let membershipService: IMembershipService;
 
   beforeEach(() => {
     membershipService = new MembershipService();
-    proxySpy = jest.spyOn(proxy, "getData");
+    proxySpy = jest.spyOn(proxy, 'getData');
     proxySpy.mockClear();
   });
 
   /**
-   * Calls getUsers() and checks a call was made to the server
+   * Calls getUsers() and checks a call was made to the proxy
    * and that the reponse contains results.
    */
-  test("should call server and return search results", done => {
-    membershipService.getUsers().then(res => {
-      expect(proxySpy).toHaveBeenCalled();
-      expect(res.length).toBeGreaterThan(0);
-      done();
-    });
+  test(`should call proxy and get search results`, async () => {
+    const res = await membershipService.getUsers();
+    expect(proxySpy).toHaveBeenCalled();
+    expect(res.length).toBeGreaterThan(0);
   });
 
   /**
    * Performs the same getUsers() call multiple times
-   * and checks that only one request was actually sent to the server.
+   * and checks that only one request was actually sent to the proxy.
    */
-  test("should throttle repeating requests until server replies", done => {
-    callServiceMultipleTimes(5).then(res => {
-      expect(proxySpy).toHaveBeenCalledTimes(1);
-      expect(res[0]).toEqual(res[1]);
-      done();
-    });
+  test(`should throttle repeating requests until the proxy returns a result`, async () => {
+    const results = await callServiceMultipleTimes(5);
+    expect(proxySpy).toHaveBeenCalledTimes(1);
+    expect(results[0]).toEqual(results[1]);
   });
 
   /**
    * Performs the same request multiple times. Once finished with the
-   * first request it does it again. Then it checks that only 2 calls were
-   * made to the server.
+   * first set of requests (and a result was returned), it does it again. Then it checks that only 2 calls were
+   * made to the proxy.
    */
-  test("should send another request when called after resolved", done => {
-    callServiceMultipleTimes(5).then(() => {
-      callServiceMultipleTimes(5);
-      expect(proxySpy).toHaveBeenCalledTimes(2);
-      done();
-    });
+  test(`should send another request when called after the first one was resolved`, async () => {
+    await callServiceMultipleTimes(5);
+    callServiceMultipleTimes(5);
+    expect(proxySpy).toHaveBeenCalledTimes(2);
   });
 
   /**
-   * Calls getUsers() with a `name` parameter and checks
-   * that the results match the name.
+   * Calls getUsers() with a "name" parameter and checks
+   * that the results are filtered to match the parameter value.
    */
-  test("should return only results that match the name query", done => {
-    const name = "dave";
-    membershipService.getUsers(name).then(results => {
-      let isResultsCorrect = resultsMatchNameQuery(results, name);
-      expect(isResultsCorrect).toBeTruthy();
-      done();
-    });
+  test(`should return only results that match the "name" query parameter`, async () => {
+    const name = 'dave';
+    const results = await membershipService.getUsers(name);
+    let isResultsCorrect = resultsMatchNameQuery(results, name);
+    expect(isResultsCorrect).toBeTruthy();
   });
 
   /**
-   * Sends 2 different calls to the server and
-   * checks 2 calls were sent to the server.
+   * Executes the getUsers() call with 2 different "name" values
+   * and then checks 2 calls were made to the proxy.
    */
-  test("should call server once per query", done => {
-    const phrase1 = "dave";
-    const phrase2 = "michael";
+  test(`should call proxy once per query`, () => {
+    const phrase1 = 'dave';
+    const phrase2 = 'michael';
     membershipService.getUsers(phrase1);
     membershipService.getUsers(phrase2);
     expect(proxySpy).toHaveBeenCalledTimes(2);
-    done();
   });
 
   /** ----------------------------------------- */
@@ -85,10 +76,8 @@ describe("MembershipService getUsers() method", () => {
    * @param {string} name
    * @returns {boolean} `boolean` - true when every result matches and false when not
    */
-  function resultsMatchNameQuery(results: IUser[], name: string) {
-    return results.every(
-      x => x.name.toLowerCase().indexOf(name.toLowerCase()) >= 0
-    );
+  function resultsMatchNameQuery(results: IUser[], name: string): boolean {
+    return results.every((x) => x.name.toLowerCase().indexOf(name.toLowerCase()) >= 0);
   }
 
   /**
@@ -99,13 +88,10 @@ describe("MembershipService getUsers() method", () => {
    * @param {string} [name]
    * @returns `Promise<IUser[][]>`
    */
-  function callServiceMultipleTimes(
-    repeat: number,
-    name?: string
-  ): Promise<IUser[][]> {
+  function callServiceMultipleTimes(repeat: number, name?: string): Promise<IUser[][]> {
     const arr: Promise<IUser[]>[] = Array(repeat)
       .fill(0)
-      .map(x => membershipService.getUsers.bind(membershipService)(name));
+      .map((x) => membershipService.getUsers.bind(membershipService)(name));
     return Promise.all(arr);
   }
 });
--- src/membership.service.ts
+++ src/membership.service.ts
@@ -1,4 +1,4 @@
-import { ProxyServiceMock } from "./proxy.service.mock";
+import { ProxyServiceMock } from './proxy.service.mock'; // <-- I am here for a reason... USE ME!
 
 export class MembershipService implements IMembershipService {
   public getUsers(): Promise<IUser[]> {
